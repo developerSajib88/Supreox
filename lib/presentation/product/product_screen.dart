dfd40236a3f7e956d6330e456c903389a9979d31
developerSajib88 <developersajib88@gmail.com>
2024-06-29 21:22:38 +0600

    Ui constant create.

---
+++ lib/presentation/product/product_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class ProductScreen extends HookConsumerWidget {
+  const ProductScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: 1.sw,
+          height: 1.sh,
+          child: ,
+        ),
+      ),
+    );
+  }
+}
