f91199c6f719f05f33b255fdb5479c3ed1801133
developerSajib88 <developersajib88@gmail.com>
2024-06-29 20:26:56 +0600

    Dependency inject and provider create for add to cart domain and Infrastructure and state management.

--- lib/core/dependency_injection/dependency_injection.dart
+++ lib/core/dependency_injection/dependency_injection.dart
@@ -1,9 +1,10 @@
 import 'package:get_it/get_it.dart';
+import 'package:supreox/application/add_to_cart/add_to_cart_state.dart';
+import 'package:supreox/domain/add_to_cart/add_to_cart_domain.dart';
+import 'package:supreox/infrastructure/add_to_cart/add_to_cart_domain_I.dart';
 
 final getIt = GetIt.instance;
 
 void init() {
-  // getIt
-  //   ..registerLazySingleton<AuthenticationDomain>(AuthenticationDomI.new)
-  //   ..registerLazySingleton<CountryDom>(CountryDomImpl.new);
+  getIt.registerLazySingleton<AddToCartDomain>(AddToCartDomainI.new);
 }
--- lib/core/dependency_injection/dependency_injection_provider.dart
+++ lib/core/dependency_injection/dependency_injection_provider.dart
@@ -1,7 +1,9 @@
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:supreox/application/add_to_cart/add_to_cart_state.dart';
+import 'package:supreox/application/add_to_cart/add_to_cart_state_notifier.dart';
+import 'package:supreox/core/dependency_injection/dependency_injection.dart';
 
-// Authentication Provider
-final authenticationProvider = StateNotifierProvider
-    .autoDispose<AuthenticationStateNotifier, AuthenticationState>((ref) =>
-        AuthenticationStateNotifier(authenticationDomain: getIt(), ref: ref));
+// Add to cart provider
+final addToCartProvider = StateNotifierProvider<AddToCartStateNotifier, AddToCartState>((ref) =>
+        AddToCartStateNotifier(authenticationDomain: getIt());
