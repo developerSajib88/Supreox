740eed1cebef35c6382b2be27054374b79d825d9
developerSajib88 <developersajib88@gmail.com>
2024-06-29 20:15:32 +0600

    State create for add to cart state management.

---
+++ lib/application/add_to_cart/add_to_cart_state_notifier.dart
@@ -0,0 +1,7 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:supreox/application/add_to_cart/add_to_cart_state.dart';
+
+class AddToCartStateNotifier extends StateNotifier<AddToCartState>{
+  AddToCartStateNotifier():super(AddToCartState());
+
+}
