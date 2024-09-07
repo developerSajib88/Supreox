f3889fdaba41595ec9d1f1813a00d0afb966dfaf
Sajib Hasan <116863592+developerSajib88@users.noreply.github.com>
2024-09-08 01:14:02 +0600

    Update product_domain_I.dart

--- lib/infrastructure/add_to_cart/product_domain_I.dart
+++ lib/infrastructure/add_to_cart/product_domain_I.dart
@@ -3,9 +3,9 @@ import 'package:supreox/data/model/product/product_data_res.dart';
 import 'package:supreox/domain/add_to_cart/product_domain.dart';
 
 class ProductDomainI implements ProductDomain{
-
+  
   @override
   Future<ProductDataRes?> getProductDataRes()async =>
       await ApiServices.getProduct();
-
+  
 }
